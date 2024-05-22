cec342cfd8521d81f71232309948037f4caa8422
malek <divamichaelis@gmail.com>
2024-05-22 22:18:26 +0100

    Malak

---
+++ src/main/java/com/example/cabinetorthophone/AjouterQCUController.java
@@ -0,0 +1,16 @@
+package com.example.cabinetorthophone;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class AjouterQCUController {
+    public void Back(MouseEvent mouseEvent) {
+    }
+
+    public void Finish(ActionEvent actionEvent) {
+    }
+
+    public void Ajouter(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/com/example/cabinetorthophone/AjouterTestQuestionController.java
@@ -0,0 +1,22 @@
+package com.example.cabinetorthophone;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class AjouterTestQuestionController {
+    public void Back(MouseEvent mouseEvent) {
+    }
+
+    public void libre(ActionEvent actionEvent) {
+    }
+
+    public void qcm(ActionEvent actionEvent) {
+    }
+
+    public void qcu(ActionEvent actionEvent) {
+    }
+
+    public void question_suivant(ActionEvent actionEvent) {
+
+    }
+}
